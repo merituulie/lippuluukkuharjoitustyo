1d5fe366376eacecea5a4bd5611959f0e7da098e
Eskelinen Meri <a1800979@myy.haaga-helia.fi>
2019-04-17 09:27:11 +0300

    Lisatty jquery depency

--- src/main/java/swd20/lippuluukku/LippuluukkuApplication.java
+++ src/main/java/swd20/lippuluukku/LippuluukkuApplication.java
@@ -2,6 +2,7 @@ package swd20.lippuluukku;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import swd20.lippuluukku.domain.Kayttaja;
